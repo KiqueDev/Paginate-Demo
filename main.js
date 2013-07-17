d93d42a7399010b52a2c6e7be8395860feea40b9
Enrique <kique10000@gmail.com>
2013-07-17 17:05:46 -0400

    Update multiple paginte demo and added home page

--- main.js
+++ main.js
@@ -1,113 +1,86 @@
 Adults = new Meteor.Collection("adults");
 Childrens = new Meteor.Collection("childrens");
-Canines = new Meteor.Collection("canines");
-Vegetarians = new Meteor.Collection("vegetarians");
-// var callback = function(){
-//     console.log("HEREE", this.Results.find(), this.coll);
-//     return this.Results.find({});
-//     // return self.Results.find({});
-// }
 
+Animals = new Meteor.Collection("animals");
 
 if (Meteor.isClient) {
-  //Default Settings
-  //var P = new Paginate(Coll);
 
-  var childrenPages = new Paginate(Childrens, "peopleTemplate", {perPage:3, resultName:"people"});
-  var Adult = new Paginate(Adults, "peopleTemplate", {perPage:4, resultName:"people" });
-  var Canine = new Paginate(Canines, "animalTemplate", {perPage:2, resultName:"animals"});
-  var Vegetarian = new Paginate(Vegetarians, "animalTemplate", {perPage:5, resultName:"animals"});
+  //SINGLE PAGINATE WITH ONE TEMPLATE
+  var animalPages = new Paginate(Animals, "animalTemplate", {perPage:2});
 
-  
-
-
-  //P1.removeAllData();
-  //P1.populateData();
-
-  // Template.peopleTemplate.people = function () {
-  //   //return Paginate.pages.results.find({});
-  //   return Adult.Results.find({});
-  // };
-
-  // Template.animalTemplate.animal = function () {
-  //   //return Paginate.p1.Results.find({});
-  //   return Animal.Results.find({});
-  // };
-
+  //MULTIPLE PAGINATE WITH ONE TEMPLATE
+  var childrenPages = new Paginate(Childrens, "peopleTemplate", {perPage:3, templateData:"people"});
+  var adultPages = new Paginate(Adults, "peopleTemplate", {perPage:4, templateData:"people" });
 
   Meteor.autorun(function() {
+    Meteor.Router.add({ 
+      '/': "home"
+    });
 
     console.log('autorun ran!');
     Meteor.subscribe("adults");
     Meteor.subscribe("childrens");
-    Meteor.subscribe("canines");
-    Meteor.subscribe("vegetarians");
+    Meteor.subscribe("animals");
   });
 
-  Meteor.startup(function () {
+  Template.home.events({
+    'click #animalBtn': function(e){
+      e.preventDefault();
+      Meteor.Router.to("/animals/1");
+    },
+    'click #adultBtn': function(e){
+      e.preventDefault();
+      Meteor.Router.to("/adults/1");
+    },
+    'click #childrenBtn': function(e){
+      e.preventDefault();
+      Meteor.Router.to("/childrens/1");
+    }
 
-    // code to run on server at startup
-
-    // console.log(childrenPages);
-  
-
-    // childrenPages.next()
-    // childrenPages.next()
-
-    // childrenPages.goTo(2)
-    // childrenPages.previous()
   });
-
 }
 
 if (Meteor.isServer) {
   Adults.remove({});
   Childrens.remove({});
-  Canines.remove({});
-  Vegetarians.remove({});
+  Animals.remove({});
 
-  Adults.insert({"name":"Jose", "location":"New York"});
-  Adults.insert({"name":"Carlos", "location":"France"});
-  Adults.insert({"name":"Nathan", "location":"New York"});
-  Adults.insert({"name":"Enrique", "location":"New Jersey"});
-  Adults.insert({"name":"Eddie", "location":"Brasil"});
-  Adults.insert({"name":"Rusian", "location":"China"});
-  Adults.insert({"name":"Josh", "location":"Costa Rica"});
-  Adults.insert({"name":"James", "location":"Hong Kong"});
+  Adults.insert({"name":"Jose", "age":"26"});
+  Adults.insert({"name":"Carlos", "age":"18"});
+  Adults.insert({"name":"Nathan", "age":"60"});
+  Adults.insert({"name":"Enrique", "age":"102"});
+  Adults.insert({"name":"Eddie", "age":"20"});
+  Adults.insert({"name":"Rusian", "age":"21"});
+  Adults.insert({"name":"Josh", "age":"45"});
+  Adults.insert({"name":"James", "age":"36"});
 
   
-  Childrens.insert({"name":"Alex", "location":"New York"});
-  Childrens.insert({"name":"Slava", "location":"Hong Kong"});
-  Childrens.insert({"name":"Mike", "location":"Malaysia"});
-  Childrens.insert({"name":"Ruth", "location":"Brooklyn"});
-  Childrens.insert({"name":"Amy", "location":"Canada"});
-  Childrens.insert({"name":"Tikky", "location":"Miami"});
-  Childrens.insert({"name":"Rosa", "location":"Albany"});
-  Childrens.insert({"name":"Lisa", "location":"Italy"});
-  Childrens.insert({"name":"Uncle", "location":"Russia"});
-  Childrens.insert({"name":"Dennis", "location":"California"});
+  Childrens.insert({"name":"Alex", "age":"2"});
+  Childrens.insert({"name":"Slava", "age":"9"});
+  Childrens.insert({"name":"Mike", "age":"3"});
+  Childrens.insert({"name":"Ruth", "age":"3"});
+  Childrens.insert({"name":"Amy", "age":"5"});
+  Childrens.insert({"name":"Tikky", "age":"4"});
+  Childrens.insert({"name":"Rosa", "age":"7"});
+  Childrens.insert({"name":"Lisa", "age":"6.5"});
+  Childrens.insert({"name":"Uncle", "age":"1"});
+  Childrens.insert({"name":"Dennis", "age":"0.5"});
 
 
-  Canines.insert({"name":"Tiger", "size":"120"});
-  Canines.insert({"name":"Sharks", "size":"400"});
-  Canines.insert({"name":"Dino", "size":"5"});
-  Canines.insert({"name":"Dog", "size":"30"});
-  Canines.insert({"name":"Rhyno", "size":"600"});
-  Canines.insert({"name":"Elephant", "size":"1555"});
-  Canines.insert({"name":"Cat", "size":"300"});
-  Canines.insert({"name":"Ants", "size":"1"});
+  Animals.insert({"name":"Tiger", "size":"120"});
+  Animals.insert({"name":"Sharks", "size":"400"});
+  Animals.insert({"name":"Dino", "size":"5"});
+  Animals.insert({"name":"Dog", "size":"30"});
+  Animals.insert({"name":"Rhyno", "size":"600"});
+  Animals.insert({"name":"Elephant", "size":"1555"});
+  Animals.insert({"name":"Cat", "size":"300"});
+  Animals.insert({"name":"Ants", "size":"1"});
+  Animals.insert({"name":"Birds", "size":"30"});
+  Animals.insert({"name":"Lol", "size":"234"});
+  Animals.insert({"name":"Elephant", "size":"234"});
+  Animals.insert({"name":"Cat", "size":"300"});
+  Animals.insert({"name":"Ants", "size":"1234"});
 
-
-  Vegetarians.insert({"name":"Birds", "size":"30"});
-  Vegetarians.insert({"name":"Lol", "size":"234"});
-  Vegetarians.insert({"name":"Elephant", "size":"234"});
-  Vegetarians.insert({"name":"Cat", "size":"300"});
-  Vegetarians.insert({"name":"Ants", "size":"1234"});
-  Vegetarians.insert({"name":"Dog", "size":"2343"});
-  Vegetarians.insert({"name":"Rhyno", "size":"602330"});
-  Vegetarians.insert({"name":"Elephant", "size":"355"});
-  Vegetarians.insert({"name":"Cat", "size":"23"});
-  Vegetarians.insert({"name":"Ants", "size":"1"});
 
 
   Meteor.publish("adults", function() { 
@@ -116,13 +89,9 @@ if (Meteor.isServer) {
   Meteor.publish("childrens", function() { 
     return Childrens.find({}, {fields: {}});
   });
-  Meteor.publish("canines", function() { 
-    return Canines.find({}, {fields: {}});
+  Meteor.publish("animals", function() { 
+    return Animals.find({}, {fields: {}});
   });
-  Meteor.publish("vegetarians", function() { 
-    return Vegetarians.find({}, {fields: {}});
-  });
-
 
 }
 
